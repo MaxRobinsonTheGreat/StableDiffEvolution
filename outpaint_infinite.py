3120dd260c4f370ca05a5363740eddf57054333c
Max <maxdrob1@gmail.com>
2023-01-16 16:07:24 -0600

    added infinite outpainting/upscaling

---
+++ outpaint_infinite.py
@@ -0,0 +1,83 @@
+import torch
+from PIL import Image
+import numpy as np
+from diffusers import StableDiffusionInpaintPipeline
+import os, json, util
+
+project_name = "cyberpunk"
+prompt_file = "./outpaint_prompt.json"
+start_image = 'cyberpunk.png'
+height = 512
+window_size = 512
+slide_size = 128
+num_options = 4
+used_prompts = []
+
+proj_dir = "./outpaints/"+project_name
+os.makedirs(proj_dir, exist_ok=True)
+
+pipe = StableDiffusionInpaintPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-2-inpainting",
+    revision="fp16",
+    torch_dtype=torch.float16,
+).to("cuda")
+
+mask = np.ones((height, window_size))
+mask[:,0:slide_size] = 0
+mask_image = Image.fromarray(np.uint8(mask*255)).convert('RGB')
+mask_image.save(proj_dir+'/mask.png')
+
+cur_image = Image.open(start_image)
+cur_image = cur_image.resize((height, window_size))
+full_image = cur_image.copy()
+i=0
+while True:
+    im = np.array(cur_image)
+    start = im.shape[1]-slide_size
+    next_im = np.zeros((height, window_size, 3))
+    next_im[:,0:slide_size] = im[:,start:]
+    next_im = Image.fromarray(np.uint8(next_im)).convert('RGB')
+    next_im.save(proj_dir+'/'+str(i)+'.png')
+    
+    cur_image=None
+    while cur_image is None:
+        with open(prompt_file) as json_file:
+            prompts = json.load(json_file)
+            prompt = prompts['prompt']
+            negative_prompt = prompts['negative_prompt']
+        ims = pipe(
+            prompt=[prompt]*num_options, 
+            negative_prompt=[negative_prompt]*num_options, 
+            image=next_im, 
+            mask_image=mask_image
+        ).images
+        im_grid = util.image_grid(ims, 1, num_options)
+        im_grid.save(proj_dir+'/_choice.png')
+        choice = input("Frame "+str(i)+" finished. Choice: ")
+        if choice == "q":
+            break
+        if choice.isdigit():
+            choice = int(choice)
+            choice-=1
+            if choice>=0 and choice<num_options:
+                cur_image=ims[choice]
+                used_prompts.append({
+                    'prompt': prompt,
+                    'neg_prompt': negative_prompt
+                })
+                with open(proj_dir+'/used_prompts.json', 'w') as fp:
+                    json.dump(used_prompts, fp)
+
+    if cur_image is None:
+        break
+
+    cur_image.save(proj_dir+'/'+str(i)+'.png')
+    
+    im = np.array(cur_image)
+    full_image = np.append(full_image, im[:,slide_size:], 1)
+    full_image = Image.fromarray(np.uint8(full_image)).convert('RGB')
+    full_image.save(proj_dir+'/full.png')
+
+    i+=1
+
+
--- upscale_wide.py
+++ upscale_wide.py
@@ -3,19 +3,21 @@ from torch import autocast
 from tqdm.auto import tqdm
 from util import toPil
 import numpy as np
-import os
+import os, json
 from math import ceil
 
 from diffusers import StableDiffusionUpscalePipeline
 
 from PIL import Image
 
-proj_name = "test"
-image_path = "./outpaints/test/full.png"
-prompt = "A boy holding a balloon animal, 8k uhd photograph"
-negative_prompt = "ugly, gross, malformed, deformed, mutilated, extra fingers, missing fingers, glitchy"
-chunk_size = 512
-resize_to = 128
+proj_name = "cyberpunk"
+image_path = "./outpaints/cyberpunk/full.png"
+prompt_file = "./outpaint_prompt.json"
+prompt = "Cyberpunk Innovative futuristic Japanese city at night, hazy and foggy, octane render, professional ominous concept art, an intricate, elegant, highly detailed digital painting, concept art, smooth, sharp focus, illustration, volumetric lighting"
+negative_prompt = "Deformed, disfigured, mutilated, gross, glitchy, messy, disorganized, watermark"
+full_chunk_size = 512
+lowres_chunk_size = 256
+resize_ratio = full_chunk_size//lowres_chunk_size
 
 model_path = "stabilityai/stable-diffusion-x4-upscaler"
 device = "cuda"
@@ -32,26 +34,39 @@ pipe = pipe.to(device)
 pipe.enable_attention_slicing()
 
 full_img = Image.open(image_path).convert("RGB")
+downsize = (full_img.width//resize_ratio, full_img.height//resize_ratio)
+full_img_downsized = full_img.resize(downsize)
 full_upscaled = np.array([])
 
-full_arr = np.array(full_img)
+full_arr = np.array(full_img_downsized)
 width = full_arr.shape[1]
-num_sub_ims = ceil(width / chunk_size)
-print(width)
+num_sub_ims = ceil(width / lowres_chunk_size)
 for i in range(num_sub_ims):
-    start = i*chunk_size
-    end = min((i+1)*chunk_size, width)
-    print(start, end)
+    start = i*lowres_chunk_size
+    end = min((i+1)*lowres_chunk_size, width)
     img = full_arr[:,start:end]
-    print(img.shape)
-    img = toPil(img)
-    img = img.resize((resize_to, resize_to))
-    img.save(proj_dir+'/temp.png')
-    upscaled = pipe(prompt=prompt, negative_prompt=negative_prompt, image=img).images[0]
-    upscaled.save(proj_dir+'/'+str(i)+'.png')
 
-    if i == 0:
-        full_upscaled = np.array(upscaled)
-    else:
-        full_upscaled = np.append(full_upscaled, np.array(upscaled), 1)
+    img = toPil(img)
+    img.save(proj_dir+'/temp.png')
+
+    while True:
+        with open(prompt_file) as json_file:
+            prompts = json.load(json_file)
+            prompt = prompts['prompt']
+            negative_prompt = prompts['negative_prompt']
+        upscaled = pipe(prompt=prompt, negative_prompt=negative_prompt, image=img).images[0]
+        upscaled.save(proj_dir+'/'+str(i)+'.png')
+
+        if i == 0:
+            temp_full_upscaled = np.array(upscaled)
+        else:
+            temp_full_upscaled = np.append(full_upscaled, np.array(upscaled), 1)
+        toPil(temp_full_upscaled).save(proj_dir+'/_staged.png')
+        user_in = input("Continue[enter] or reroll[r]?")
+        if user_in == "":
+            full_upscaled=temp_full_upscaled
+            break
+
     toPil(full_upscaled).save(proj_dir+'/full.png')
+
+
